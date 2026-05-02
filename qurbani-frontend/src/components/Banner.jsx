9c5f13659ecc13745aacfdbed209123e3c3375f5
Nahid711811 <nmdnahidhasan5@gmail.com>
2026-05-02 22:36:46 +0600

    added animal json data & banner

---
+++ qurbani-frontend/src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className='flex justify-between gap-4 p-1 mt-10 bg-green-700'>
+      <div className='flex flex-col justify-start items-start gap-6'>
+        <div className='p-3 rounded-3xl bg-yellow-300'>
+            <p className='text-center text-2xl'>QurbaniHat Marketplace</p>
+        </div>
+        <p className='text-4xl text-white font-bold'>Premium Livestock Booking for Qurbani</p>
+        <p className='text-xl'>Explore selected cows and goats with clear price, breed, weight, age and location before booking.</p>
+      </div>
+      <div>
+        <img src="https://img.magnific.com/free-photo/cows-eating-lush-grass-green-field-front-fuji-mountain-japan_335224-36.jpg?semt=ais_hybrid&w=740&q=80" alt="cow image" />
+      </div>
+    </div>
+  )
+}
--- qurbani-frontend/src/pages/Home.jsx
+++ qurbani-frontend/src/pages/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Banner from '../components/Banner'
 
 export default function Home() {
+     const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    fetch("/animals.json")
+      .then((res) => res.json())
+      .then((data) => setAnimals(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
-      home
+      <Banner />
+      <br />
+      <br />
+      <br />
+
+<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+  {animals.map((animal) => (
+    <div
+      key={animal.id}
+      className="border p-4 rounded-lg shadow flex flex-col gap-2"
+    >
+      <img
+        src={animal.image}
+        alt={animal.name}
+        className="w-full h-40 object-cover rounded"
+      />
+
+      <h3 className="text-lg font-bold">{animal.name}</h3>
+      <p>Price: {animal.price}</p>
+      <p>Type: {animal.type}</p>
+      <p>Breed: {animal.breed}</p>
+      <p>Weight: {animal.weight} kg</p>
+      <p>Age: {animal.age} years</p>
+    </div>
+  ))}
+</div>
     </div>
   )
 }
