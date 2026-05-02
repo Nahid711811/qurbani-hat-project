ac1b5145cb8ab614fc618a75f54a62bd71671c93
Nahid711811 <nmdnahidhasan5@gmail.com>
2026-05-03 03:12:37 +0600

    update home & banner page with styles

--- qurbani-frontend/src/components/Banner.jsx
+++ qurbani-frontend/src/components/Banner.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Banner() {
   return (
-    <div className='flex justify-between gap-4 p-1 mt-10 bg-[#468432]'>
+    <div className='flex flex-col py-8 px-6 md:flex-row lg:flex-row justify-between gap-4 md:py-20 md:px-16 mt-10 bg-[#468432]'>
       <div className='flex flex-col justify-start items-start gap-6'>
-        <div className='p-3 rounded-3xl bg-[#FFA02E]'>
-            <p className='text-center text-2xl'>QurbaniHat Marketplace</p>
+        <div className='p-3 rounded-xl bg-[#FFA02E]'>
+            <p className='text-center text-lg md:text-2xl'>QurbaniHat Marketplace</p>
         </div>
         <p className='text-4xl text-white font-bold'>Premium Livestock Booking for Qurbani</p>
-        <p className='text-xl'>Explore selected cows and goats with clear price, breed, weight, age and location before booking.</p>
+        <p className='text-lg'>Explore selected cows and goats with clear price, breed, weight, age and location before booking.</p>
       </div>
-      <div>
-        <img src="https://img.magnific.com/free-photo/cows-eating-lush-grass-green-field-front-fuji-mountain-japan_335224-36.jpg?semt=ais_hybrid&w=740&q=80" alt="cow image" />
+      <div className='rounded-lg'>
+        <img className='rounded-lg' src="https://img.magnific.com/free-photo/cows-eating-lush-grass-green-field-front-fuji-mountain-japan_335224-36.jpg?semt=ais_hybrid&w=740&q=80" alt="cow image" />
       </div>
     </div>
   )
--- qurbani-frontend/src/pages/Home.jsx
+++ qurbani-frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Banner from '../components/Banner'
+import {Link} from "react-router-dom"
 
 export default function Home() {
      const [animals, setAnimals] = useState([]);
-    //  const aniFil = filter(ani=>ani.cow == "cow")
-    //  console.log("anifil", anifil)
+     const aniCow = animals.slice(0, 6)
 
   useEffect(() => {
     fetch("/animals.json")
@@ -15,30 +15,49 @@ export default function Home() {
   return (
     <div>
       <Banner />
-      <br />
-      <br />
-      <br />
 
-<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-  {animals.map((animal) => (
+<div className='bg-white mt-20 p-2'>
+    <div className='flex justify-between items-center gap-4 mb-4'>
+      <p className='text-[#FFA02E] text-3xl font-bold'>Popular Collections</p>
+      <Link
+  to="/all_animals"
+  className="py-2 px-4 rounded-2xl font-bold bg-[#468432] text-white"
+>
+  View All
+</Link>
+    </div>
+<div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+
+  {aniCow?.map((animal) => (
     <div
       key={animal.id}
-      className="border p-4 rounded-lg shadow flex flex-col gap-2"
+      className="border border-[#468432] p-4 rounded-lg shadow flex flex-col gap-2 text-md hover:shadow-lg"
     >
-      <img
+     <div>
+       <img
         src={animal.image}
         alt={animal.name}
         className="w-full h-40 object-cover rounded"
       />
+     </div>
+      <div className='flex justify-between gap-4 items-center'>
+      <p className='text-sm text-white py-1 px-2 rounded-2xl bg-[#468432]'>{animal.type}</p>
+      <p className='text-sm py-1 px-2 rounded-2xl bg-[#FFA02E]'>{animal.breed}</p>
 
-      <h3 className="text-lg font-bold">{animal.name}</h3>
-      <p>Price: {animal.price}</p>
-      <p>Type: {animal.type}</p>
-      <p>Breed: {animal.breed}</p>
+      </div>
+      <div>
+      <h3 className="text-lg font-bold text-[#468432]">{animal.name}</h3>
       <p>Weight: {animal.weight} kg</p>
       <p>Age: {animal.age} years</p>
+      <p>Location: {animal.location}</p>
+      </div>
+      <div className='flex justify-between gap-4 items-center'>
+      <p className='font-bold text-[#FFA02E]'>৳{animal.price}</p>
+      <button className='bg-[#468432] font-bold p-2 rounded-xl text-white hover:cursor-pointer'>Details</button>
+      </div>
     </div>
   ))}
+</div>
 </div>
     </div>
   )
